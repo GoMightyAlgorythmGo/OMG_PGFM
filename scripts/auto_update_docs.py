8f6819f793ee6dd80372ed7faf3dfb269709e4fc
hanskraut31312312 <51217158+hanskraut31312312@users.noreply.github.com>
2024-05-25 13:53:04 +0200

    Update GitHub Actions workflow and scripts

--- scripts/auto_update_docs.py
+++ scripts/auto_update_docs.py
@@ -5,9 +5,14 @@ def generate_docs():
     os.system('sphinx-build -b html docs/source docs/build')
 
 def git_commit_push():
+    github_token = os.getenv('MY_GITHUB_TOKEN')
     subprocess.run(['git', 'add', 'docs/build'])
     subprocess.run(['git', 'commit', '-m', 'Automated documentation update'])
-    subprocess.run(['git', 'push', 'origin', 'master'])
+    subprocess.run([
+        'git', 'push',
+        'https://<GITHUB_USERNAME>:' + github_token + '@github.com/<GITHUB_USERNAME>/<REPO_NAME>.git',
+        'master'
+    ])
 
 if __name__ == "__main__":
     generate_docs()
